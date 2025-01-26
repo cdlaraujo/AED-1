73caa83b80785453736a542fca9c3f31a08384c5
ndgerenciamentodeprojetos <164252566+cdlaraujo@users.noreply.github.com>
2025-01-26 19:29:46 -0300

    Update 1152.c

--- 1152.c
+++ 1152.c
@@ -1,104 +1,75 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct UniaoBusca
-{
-    int n;
-    int *P, *ranking;
-};
+typedef struct {
+    int u, v, weight;
+} Edge;
 
-void inicializa(struct UniaoBusca *UB, int n){
-    UB->n = n;
-    UB->P = (int*)malloc(n * sizeof(int));
-    UB->ranking = (int*)malloc(n * sizeof(int));
-    for(int i = 0; i < n; ++i){
-        UB->P[i] = i;
-    }
+int compareEdges(const void *a, const void *b) {
+    Edge *edge1 = (Edge *)a;
+    Edge *edge2 = (Edge *)b;
+    return edge1->weight - edge2->weight;
 }
 
-void destroi(struct UniaoBusca *UB){
-    UB->n = 0;
-    free(UB->P);
-    free(UB->ranking);
+int find(int parent[], int x) {
+    if (parent[x] != x)
+        parent[x] = find(parent, parent[x]); // Path compression
+    return parent[x];
 }
 
-int encontraConjunto(struct UniaoBusca *UB, int i){
-    if(UB->P[i] == i){
-        return i;
-    }
-    UB->P[i] = encontraConjunto(UB, UB->P[i]);
-    return UB->P[i];
-}
+void unionSets(int parent[], int rank[], int x, int y) {
+    int rootX = find(parent, x);
+    int rootY = find(parent, y);
 
-int mesmoConjunto(struct UniaoBusca *UB, int i, int j){
-    return encontraConjunto(UB, i) == encontraConjunto(UB, j);
-}
-
-void uneConjuntos(struct UniaoBusca *UB, int i, int j){
-    if (!mesmoConjunto(UB, i, j)){
-        int x = encontraConjunto(UB, i), y = encontraConjunto(UB, j);
-        if(UB->ranking[x] > UB->ranking[y]){
-            UB->P[y] = x;
-        }else{
-            UB->P[x] = y;
-            if(UB->ranking[x] == UB->ranking[y]){
-                UB->ranking[y]++;
-            }
+    if (rootX != rootY) {
+        if (rank[rootX] > rank[rootY]) {
+            parent[rootY] = rootX;
+        } else if (rank[rootX] < rank[rootY]) {
+            parent[rootX] = rootY;
+        } else {
+            parent[rootY] = rootX;
+            rank[rootX]++;
         }
     }
-}
-
-struct Aresta
-{
-    int u, v, peso;
-};
-
-int comp(const void *a, const void *b){
-    return ((const struct Aresta*)a)->peso - ((const struct Aresta*)b)->peso;
-}
-
-int Kruskal(struct Aresta *grafo, int n, int m){
-    int i, resposta = 0;
-    qsort(grafo, m, sizeof(struct Aresta), comp);
-
-    struct UniaoBusca ub;
-    inicializa(&ub, n);
-    for(i = 0; i < m; ++i){
-        struct Aresta atual = grafo[i];
-        if(!mesmoConjunto(&ub, atual.u, atual.v)){
-            resposta += atual.peso;
-            uneConjuntos(&ub, atual.u, atual.v);
-        }
-    }
-    destroi(&ub);
-
-    return resposta;
 }
 
 int main() {
-    int i, n, m, u, v, p;
-    struct Aresta *grafo;
+    int m, n;
+    while (scanf("%d %d", &m, &n), m || n) {
+        Edge edges[n];
+        long long totalCost = 0;
 
-    while(scanf("%d %d\n", &n, &m) != EOF){
-        if(n == 0 && m == 0){
-            break;
+        for (int i = 0; i < n; i++) {
+            scanf("%d %d %d", &edges[i].u, &edges[i].v, &edges[i].weight);
+            totalCost += edges[i].weight;
         }
-        
-        struct Aresta *grafo = (struct Aresta *)malloc(m * sizeof(struct Aresta));
 
-        int resposta = 0;
-        for(i = 0; i < m; ++i){
-            scanf("%d %d %d\n", &u, &v, &p);
-            grafo[i].u = u;
-            grafo[i].v = v;
-            grafo[i].peso = p;
-            resposta += grafo[i].peso;
+        // Sort edges by weight
+        qsort(edges, n, sizeof(Edge), compareEdges);
+
+        // Kruskal's algorithm
+        int parent[m], rank[m];
+        for (int i = 0; i < m; i++) {
+            parent[i] = i;
+            rank[i] = 0;
         }
-        resposta -= Kruskal(grafo, n, m);
 
-        printf("%d\n", resposta);
+        long long mstCost = 0;
+        int edgesUsed = 0;
 
-        free(grafo);
+        for (int i = 0; i < n && edgesUsed < m - 1; i++) {
+            int u = edges[i].u;
+            int v = edges[i].v;
+
+            if (find(parent, u) != find(parent, v)) {
+                unionSets(parent, rank, u, v);
+                mstCost += edges[i].weight;
+                edgesUsed++;
+            }
+        }
+
+        // Maximum savings
+        printf("%lld\n", totalCost - mstCost);
     }
 
     return 0;
