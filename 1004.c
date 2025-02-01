510683510265035f7033cb8b65105f4a792bdacf
ndgerenciamentodeprojetos <164252566+cdlaraujo@users.noreply.github.com>
2025-02-01 16:10:15 -0300

    Create 1004.c

---
+++ 1004.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+int main() {
+    int N, M;
+    
+    scanf("%d", &N);
+    scanf("%d", &M);
+    
+    int PROD = N * M;
+    
+    printf("PROD = %d\n", PROD);
+ 
+    return 0;
+}
