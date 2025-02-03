2717ba46c75bacc5d8dac2009d926ca56e8ead63
ndgerenciamentodeprojetos <164252566+cdlaraujo@users.noreply.github.com>
2025-02-03 20:05:27 -0300

    Update 1006.c

--- 1006.c
+++ 1006.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    int A, B, C;
-    float AVG, SUM;
-
-    scanf("%d %d %d", &A, &B, &C);
-
-    SUM = A * 2 + B * 3 + C * 5;
-    AVG = SUM / (2 + 3 + 5);
-
-    printf("MEDIA = %.5f\n", AVG);
-
+    double A, B, C;  // Changed to double for decimal inputs
+    double MEDIA;
+    
+    scanf("%lf %lf %lf", &A, &B, &C);  // Use %lf for double
+    
+    MEDIA = (A * 2 + B * 3 + C * 5) / 10.0;
+    
+    printf("MEDIA = %.1f\n", MEDIA);  // Exactly 1 decimal place
+    
     return 0;
 }
