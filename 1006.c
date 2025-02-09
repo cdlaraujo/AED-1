e0fd3f898337b33e5a1f5f9004891d3b3964ff46
ndgerenciamentodeprojetos <164252566+cdlaraujo@users.noreply.github.com>
2025-02-09 14:58:18 -0300

    Update 1006.c

--- 1006.c
+++ 1006.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    double A, B, C;  // Changed to double for decimal inputs
+    double A, B, C;
     double MEDIA;
     
-    scanf("%lf %lf %lf", &A, &B, &C);  // Use %lf for double
+    scanf("%lf %lf %lf", &A, &B, &C); 
     
     MEDIA = (A * 2 + B * 3 + C * 5) / 10.0;
     
-    printf("MEDIA = %.1f\n", MEDIA);  // Exactly 1 decimal place
+    printf("MEDIA = %.1f\n", MEDIA); 
     
     return 0;
 }
