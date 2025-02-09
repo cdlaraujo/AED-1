0cefa8dfe7351489595e0f7b19a52c2d55fac4e4
ndgerenciamentodeprojetos <164252566+cdlaraujo@users.noreply.github.com>
2025-02-09 14:56:33 -0300

    Update 1069.c

--- 1069.c
+++ 1069.c
@@ -3,37 +3,32 @@
 
 int main(){
     int number_lines = 0;
-    scanf("%d", &number_lines);  // Read the number of test cases
+    scanf("%d", &number_lines);  // numero de casos
 
-    int* answer = (int*)malloc(sizeof(int) * number_lines);  // Allocate memory for the answers
+    int* answer = (int*)malloc(sizeof(int) * number_lines); 
 
     for (int i = 0; i < number_lines; ++i) {
         int number_left = 0, number_right = 0;
         char temp;
         
-        // Consume the newline after the number of test cases and before each string.
-        while ((temp = getchar()) != '\n' && temp != EOF);  // Skip any leftover characters (like a newline)
-
-        // Read each test case line
+        while ((temp = getchar()) != '\n' && temp != EOF);  
         while ((temp = getchar()) != '\n' && temp != EOF) {
             if (temp == '<') ++number_left;
             else if (temp == '>') ++number_right;
         }
 
-        // Calculate the number of diamonds
         answer[i] = 0;
         while (number_left > 0 && number_right > 0) {
             number_left--;
             number_right--;
-            answer[i]++;  // Form one diamond
+            answer[i]++; 
         }
     }
 
-    // Print the results
     for (int i = 0; i < number_lines; ++i) {
         printf("%d\n", answer[i]);
     }
 
-    free(answer);  // Don't forget to free the allocated memory
+    free(answer);  
     return 0;
 }
