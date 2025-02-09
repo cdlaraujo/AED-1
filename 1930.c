d6b87b9232b13f45e68d2c5e04ea985c8b1602a3
ndgerenciamentodeprojetos <164252566+cdlaraujo@users.noreply.github.com>
2025-02-09 14:12:31 -0300

    Update 1930.c

--- 1930.c
+++ 1930.c
@@ -2,14 +2,8 @@
 
 int main() {
     int n1, n2, n3, n4;
-
-    // Leitura dos números de entrada separados por espaço
     scanf("%d %d %d %d", &n1, &n2, &n3, &n4);
-
-    // Calculando o número máximo de dispositivos que podem ser conectados
     int result = n1 + n2 + n3 + n4 - 3;
-
-    // Imprimindo o resultado
     printf("%d\n", result);
 
     return 0;
