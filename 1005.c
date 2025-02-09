5171ba03330849adf83eb1aa989247c4d8a53ded
ndgerenciamentodeprojetos <164252566+cdlaraujo@users.noreply.github.com>
2025-02-09 14:58:29 -0300

    Update 1005.c

--- 1005.c
+++ 1005.c
@@ -3,14 +3,11 @@
 int main() {
     double A, B, AVG;
 
-    // Lê os valores de A e B
-    scanf("%lf", &A); // Usamos %lf para ler um double
+    scanf("%lf", &A); 
     scanf("%lf", &B);
 
-    // Calcula a média ponderada
     AVG = (A * 3.5 + B * 7.5) / (3.5 + 7.5);
 
-    // Imprime o resultado com 5 casas decimais
     printf("MEDIA = %.5lf\n", AVG);
 
     return 0;
