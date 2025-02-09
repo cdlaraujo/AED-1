5b0b93af71e80c90d09560c6b55253e1d374e07a
ndgerenciamentodeprojetos <164252566+cdlaraujo@users.noreply.github.com>
2025-02-09 14:10:25 -0300

    Update 2929.c

--- 2929.c
+++ 2929.c
@@ -1,38 +1,65 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int minimos[1000000];
+#define MAX_SIZE 1000000
 
-int main(){
-    char operacao[4];
-    int N, V, minimo, p;
+// Pilha para armazenar os valores e uma auxiliar para manter o mínimo
+int stack[MAX_SIZE];     
+int minStack[MAX_SIZE]; 
+int top = -1;
 
-    p = -1;
-    scanf("%d\n", &N);
-    for(int i = 0; i < N; ++i){
-    	scanf("%s", &operacao);
+void push(int value) {
+    int currentMin = value;
+    if (top != -1) {
+        if (minStack[top] < value)
+            currentMin = minStack[top];
+    }
+    stack[++top] = value;
+    minStack[top] = currentMin;
+}
 
-        if(!memcmp(operacao, "PUSH", 4)){
-        	scanf("%d\n", &V);
+void pop() {
+    if (top == -1) {
+        printf("EMPTY\n");
+    } else {
+        top--;
+    }
+}
 
-            if(p == -1){
-                minimo = V;
-            }else{
-                minimo = (minimos[p] < V ? minimos[p] : V);
+void printMin() {
+    if (top == -1) {
+        printf("EMPTY\n");
+    } else {
+        printf("%d\n", minStack[top]);
+    }
+}
+
+int main(void) {
+    int N, value;
+    char op[5]; // 5 posições para comportar "PUSH", "POP", "MIN" e terminador '\0'
+
+    if (scanf("%d", &N) != 1) {
+        return 4;
+    }
+
+    while (N--) {
+        if (scanf("%s", op) != 1) {
+            return 3;
+        }
+
+        // Compara as strings usando strcmp
+        if (strcmp(op, "PUSH") == 0) {
+            if (scanf("%d", &value) != 1) {
+                return 1;
             }
-
-            minimos[++p] = minimo;
-        }else if(!memcmp(operacao, "POP", 3)){
-            if(p == -1){
-            	printf("EMPTY\n");
-            }else{
-                --p;
-            }
-        }else{
-            if(p == -1){
-            	printf("EMPTY\n");
-            }else{
-            	printf("%d\n", minimos[p]);
-            }
+            push(value);
+        } else if (strcmp(op, "POP") == 0) {
+            pop();
+        } else if (strcmp(op, "MIN") == 0) {
+            printMin();
+        } else {
+            return 2;
         }
     }
 
