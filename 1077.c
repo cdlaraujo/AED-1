d978647c6e4e4f4e588cca1706a7987bc2be0d06
ndgerenciamentodeprojetos <164252566+cdlaraujo@users.noreply.github.com>
2025-02-09 14:55:42 -0300

    Update 1077.c

--- 1077.c
+++ 1077.c
@@ -1,109 +1,61 @@
-#include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
-struct pilhaNo{
-    char valor;
-    struct pilhaNo* abaixo;
-};
+#define MAX 1000
 
-struct pilha{
-    int tamanho;
-    struct pilhaNo* topo;
-};
-
-void push(struct pilha* p, char valor){
-    p->tamanho += 1;
-    struct pilhaNo* novoTopo = (struct pilhaNo*) malloc(sizeof(struct pilhaNo));
-
-    novoTopo->valor = valor;
-    novoTopo->abaixo = p->topo;
-    p->topo = novoTopo;
+// op == operador
+int precedence(char op) {
+    if(op == '+' || op == '-') return 1;
+    if(op == '*' || op == '/') return 2;
+    if(op == '^') return 3;
+    return 0;
 }
 
-void pop(struct pilha* p){
-    if(p->tamanho > 0){
-        p->tamanho -= 1;
-        struct pilhaNo* velhoTopo = p->topo;
-        p->topo = p->topo->abaixo;
-        free(velhoTopo);
-    }
-}
+void infixToPostfix(const char *expr) {
+    char stack[MAX];  
+    int top = -1;    
 
-char top(struct pilha* p){
-    return p->topo->valor;
-}
+    for (int i = 0; expr[i] != '\0'; i++) {
+        char c = expr[i];
 
-int size(struct pilha* p){
-    return p->tamanho;
-}
-
-int empty(struct pilha* p){
-    return p->tamanho == 0;
-}
-
-void inicializa(struct pilha* p){
-    p->tamanho = 0;
-    p->topo = NULL;
-}
-
-void destroi(struct pilha* p){
-    while(!empty(p)){
-        pop(p);
-    }
-}
-
-int precedencia(char operador){
-    switch(operador){
-        case '+':
-        case '-':   return 1;
-        case '*':
-        case '/':   return 2;
-    }
-}
-
-int main(){
-    int N, tam;
-    struct pilha p;
-    char expressao[301];
-
-    scanf("%d\n", &N);
-
-    for(int k = 0; k < N; ++k){
-        scanf("%s\n", &expressao);
-
-        inicializa(&p);
-
-        tam = strlen(expressao);
-        for(int i = 0; i < tam; ++i){
-            if(isalpha(expressao[i]) || isdigit(expressao[i])){
-                printf("%c", expressao[i]);
-            }else if(expressao[i] == '(' || expressao[i] == '^'){
-                push(&p, expressao[i]);
-            }else if(expressao[i] == ')'){
-                while(!empty(&p) && top(&p) != '('){
-                    printf("%c", top(&p));
-                    pop(&p);
-                }
-
-                if(!empty(&p))  pop(&p);
-            }else{
-                while(!empty(&p) && top(&p) != '(' && precedencia(expressao[i]) <= precedencia(top(&p))){
-                    printf("%c", top(&p));
-                    pop(&p);
-                }
-
-                push(&p, expressao[i]);
+        if (isalnum(c)) {
+            printf("%c", c);
+        }
+        else if (c == '+' || c == '-' || c == '*' || c == '/' || c == '^') {
+            while (top != -1 && precedence(stack[top]) >= precedence(c)) {
+                printf("%c", stack[top]);
+                top--;
             }
+            stack[++top] = c;
         }
-
-        while(!empty(&p)){
-            printf("%c", top(&p));
-            pop(&p);
+        else if (c == '(') {
+            stack[++top] = c;
         }
-        printf("\n");
+        else if (c == ')') {
+            while (top != -1 && stack[top] != '(') {
+                printf("%c", stack[top]);
+                top--;
+            }
+            if (top != -1)
+                top--;
+        }
     }
+    while (top != -1) {
+        printf("%c", stack[top]);
+        top--;
+    }
+    printf("\n");
+}
 
+int main() {
+    int t;
+    char expr[400]; 
+
+    scanf("%d", &t);
+    while(t--) {
+        scanf("%s", expr);
+        infixToPostfix(expr);
+    }
     return 0;
 }
