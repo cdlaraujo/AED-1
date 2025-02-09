31c9664ad8257e5e6149d5bf3e144ae95862c45c
ndgerenciamentodeprojetos <164252566+cdlaraujo@users.noreply.github.com>
2025-02-09 14:17:37 -0300

    Update 1281.c

--- 1281.c
+++ 1281.c
@@ -9,32 +9,29 @@ typedef struct {
 } Product;
 
 int main() {
-    int N; // Number of test cases
+    int N; 
     scanf("%d", &N);
 
     while (N--) {
-        int M; // Number of products in the market
+        int M; 
         scanf("%d", &M);
 
         Product market[MAX_PRODUCTS];
 
-        // Read the market price list
         for (int i = 0; i < M; i++) {
             scanf("%s %lf", market[i].name, &market[i].price);
         }
 
-        int P; // Number of products Ms. Parcinova wants to buy
+        int P; 
         scanf("%d", &P);
 
         double totalCost = 0.0;
 
-        // Process Ms. Parcinova's shopping list
         for (int i = 0; i < P; i++) {
             char itemName[51];
             int quantity;
             scanf("%s %d", itemName, &quantity);
 
-            // Find the price of the item in the market list
             for (int j = 0; j < M; j++) {
                 if (strcmp(itemName, market[j].name) == 0) {
                     totalCost += market[j].price * quantity;
@@ -43,7 +40,6 @@ int main() {
             }
         }
 
-        // Print the total cost for the current test case
         printf("R$ %.2f\n", totalCost);
     }
 
