2fd767a106c122e6d851d0bc7c5350f9c1ecb748
ndgerenciamentodeprojetos <164252566+cdlaraujo@users.noreply.github.com>
2025-02-10 17:19:50 -0300

    Update 1110.c

--- 1110.c
+++ 1110.c
@@ -1,131 +1,298 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-struct dequeNo{
-    int valor;
-    struct dequeNo *anterior, *proximo;
+struct DequeNode {
+    int val;
+    struct DequeNode *prev;
+    struct DequeNode *next;
 };
 
-struct deque{
-    int tamanho;
-    struct dequeNo *frente, *tras;
+struct Deque {
+    int size; 
+    struct DequeNode *front; 
+    struct DequeNode *back;  
 };
 
-void push_front(struct deque* d, int valor){
-    d->tamanho += 1;
-    struct dequeNo* novoNo = (struct dequeNo*) malloc(sizeof(struct dequeNo));
-    novoNo->valor = valor;
-    novoNo->anterior = NULL;
-    novoNo->proximo = d->frente;
+// Adicionar
+void push_front(struct Deque* dq, int num) {
+    dq->size++;
+    struct DequeNode* new_node = malloc(sizeof(struct DequeNode));
+    new_node->val = num;
+    new_node->prev = NULL;
+    new_node->next = dq->front;
 
-    if(d->frente != NULL){
-        d->frente->anterior = novoNo;
+    if(dq->front != NULL) {
+        dq->front->prev = new_node;
     }
-    d->frente = novoNo;
+    dq->front = new_node;
 
-    if(d->tras == NULL){
-        d->tras = novoNo;
-    }
-}
-
-void push_back(struct deque* d, int valor){
-    d->tamanho += 1;
-    struct dequeNo* novoNo = (struct dequeNo*) malloc(sizeof(struct dequeNo));
-    novoNo->valor = valor;
-    novoNo->anterior = d->tras;
-    novoNo->proximo = NULL;
-
-    if(d->tras != NULL){
-        d->tras->proximo = novoNo;
-    }
-    d->tras = novoNo;
-
-    if(d->frente == NULL){
-        d->frente = novoNo;
+    if(dq->back == NULL) {
+        dq->back = new_node;
     }
 }
 
-void pop_front(struct deque* d){
-    if(d->tamanho > 0){
-        d->tamanho -= 1;
-        struct dequeNo* velhaFrente = d->frente;
-        d->frente = velhaFrente->proximo;
-        free(velhaFrente);
+// d#include <stdlib.h>
+#include <stdio.h>
+
+// Double-linked list node
+struct DequeNode {
+    int val;
+    struct DequeNode *prev;
+    struct DequeNode *next;
+};
+
+struct Deque {
+    int size;              
+    struct DequeNode *front; 
+    struct DequeNode *back;  
+};
+
+// Adicionar ao inicio
+void push_front(struct Deque* dq, int num) {
+    dq->size++;
+    struct DequeNode* new_node = malloc(sizeof(struct DequeNode));
+    new_node->val = num;
+    new_node->prev = NULL;
+    new_node->next = dq->front;
+
+    if(dq->front != NULL) {
+        dq->front->prev = new_node;
+    }
+    dq->front = new_node;
+
+    // First node case
+    if(dq->back == NULL) {
+        dq->back = new_node;
     }
 }
 
-void pop_back(struct deque* d){
-    if(d->tamanho > 0){
-        d->tamanho -= 1;
-        struct dequeNo* velhoTras = d->tras;
-        d->tras = velhoTras->anterior;
-        free(velhoTras);
+// Adicionar ao final
+void push_back(struct Deque* dq, int num) {
+    dq->size++;
+    struct DequeNode* new_node = malloc(sizeof(struct DequeNode));
+    new_node->val = num;
+    new_node->prev = dq->back;
+    new_node->next = NULL;
+
+    if(dq->back != NULL) {
+        dq->back->next = new_node;
+    }
+    dq->back = new_node;
+
+    if(dq->front == NULL) {
+        dq->front = new_node;
     }
 }
 
-int front(struct deque* d){
-    return d->frente->valor;
+// remover do incio
+void pop_front(struct Deque* dq) {
+    if(dq->size == 0) return;  
+    
+    dq->size--;
+    struct DequeNode* old_front = dq->front;
+    dq->front = old_front->next;
+    
+    if(dq->front != NULL) {
+        dq->front->prev = NULL;
+    } else {
+        dq->back = NULL; 
+    }
+    
+    free(old_front);
 }
 
-int back(struct deque* d){
-    return d->tras->valor;
+void pop_back(struct Deque* dq) {
+    if(dq->size == 0) return;
+    
+    dq->size--;
+    struct DequeNode* old_back = dq->back;
+    dq->back = old_back->prev;
+    
+    if(dq->back != NULL) {
+        dq->back->next = NULL;
+    } else {
+        dq->front = NULL; 
+    }
+    
+    free(old_back);
 }
 
-int size(struct deque* d){
-    return d->tamanho;
+int front_val(struct Deque* dq) {
+    return dq->front->val;
 }
 
-int empty(struct deque* d){
-    return d->tamanho == 0;
+int back_val(struct Deque* dq) {
+    return dq->back->val;
 }
 
-void inicializa(struct deque* d){
-    d->tamanho = 0;
-    d->frente = NULL;
-    d->tras = NULL;
+int deque_size(struct Deque* dq) {
+    return dq->size;
 }
 
-void destroi(struct deque* d){
-    while(!empty(d)){
-        pop_front(d);
+int is_empty(struct Deque* dq) {
+    return dq->size == 0;
+}
+
+void init_deque(struct Deque* dq) {
+    dq->size = 0;
+    dq->front = dq->back = NULL;
+}
+
+void cleanup_deque(struct Deque* dq) {
+    while(!is_empty(dq)) {
+        pop_front(dq); 
     }
 }
 
-int main()
-{
+int main() {
     int n, first;
-    struct deque cartas;
+    struct Deque deck; 
 
-    while (scanf("%d", &n))
-    {
-        if (!n)
-            break;
-
-        inicializa(&cartas);
-
-        for (int i = 1; i <= n; ++i)
-        {
-            push_back(&cartas, i);
+    while(1) {
+        scanf("%d", &n);
+        if(n == 0) break; 
+        
+        init_deque(&deck);
+        
+        for(int i = 1; i <= n; i++) {
+            push_back(&deck, i);
         }
-
-        first = 1;
+        
         printf("Discarded cards: ");
-        while (size(&cartas) > 1)
-        {
-            if (!first)
-                printf(", ");
-            else
+        first = 1; 
+        
+        while(deque_size(&deck) > 1) {
+            if(!first) {
+                printf(", "); 
+            } else {
                 first = 0;
-
-            printf("%d", front(&cartas));
-
-            pop_front(&cartas);
-            push_back(&cartas, front(&cartas));
-            pop_front(&cartas);
+            }
+            
+            printf("%d", front_val(&deck));
+            pop_front(&deck);
+            
+            push_back(&deck, front_val(&deck));
+            pop_front(&deck);
         }
-        printf("\nRemaining card: %d\n", front(&cartas));
+        
+        printf("\nRemaining card: %d\n", front_val(&deck));
+        cleanup_deque(&deck); 
+    }
 
-        destroi(&cartas);
+    return 0;
+}A
+void push_back(struct Deque* dq, int num) {
+    dq->size++;
+    struct DequeNode* new_node = malloc(sizeof(struct DequeNode));
+    new_node->val = num;
+    new_node->prev = dq->back;
+    new_node->next = NULL;
+
+    if(dq->back != NULL) {
+        dq->back->next = new_node;
+    }
+    dq->back = new_node;
+
+    // First node case
+    if(dq->front == NULL) {
+        dq->front = new_node;
+    }
+}
+
+void pop_front(struct Deque* dq) {
+    if(dq->size == 0) return;  
+    
+    dq->size--;
+    struct DequeNode* old_front = dq->front;
+    dq->front = old_front->next;
+    
+    if(dq->front != NULL) {
+        dq->front->prev = NULL;
+    } else {
+        dq->back = NULL; 
+    }
+    
+    free(old_front);
+}
+
+
+void pop_back(struct Deque* dq) {
+    if(dq->size == 0) return;
+    
+    dq->size--;
+    struct DequeNode* old_back = dq->back;
+    dq->back = old_back->prev;
+    
+    if(dq->back != NULL) {
+        dq->back->next = NULL;
+    } else {
+        dq->front = NULL;
+    }
+    
+    free(old_back);
+}
+
+int front_val(struct Deque* dq) {
+    return dq->front->val;
+}
+
+int back_val(struct Deque* dq) {
+    return dq->back->val;
+}
+
+int deque_size(struct Deque* dq) {
+    return dq->size;
+}
+
+int is_empty(struct Deque* dq) {
+    return dq->size == 0;
+}
+
+void init_deque(struct Deque* dq) {
+    dq->size = 0;
+    dq->front = dq->back = NULL;
+}
+
+// Limpar tudo
+void cleanup_deque(struct Deque* dq) {
+    while(!is_empty(dq)) {
+        pop_front(dq); 
+    }
+}
+
+int main() {
+    int n, first;
+    struct Deque deck;
+
+    while(1) {
+        scanf("%d", &n);
+        if(n == 0) break; 
+        
+        init_deque(&deck);
+        
+        for(int i = 1; i <= n; i++) {
+            push_back(&deck, i);
+        }
+        
+        printf("Discarded cards: ");
+        first = 1; // um flag
+        
+        while(deque_size(&deck) > 1) {
+            if(!first) {
+                printf(", ");
+            } else {
+                first = 0;
+            }
+            
+            printf("%d", front_val(&deck)); 
+            pop_front(&deck);
+            
+            push_back(&deck, front_val(&deck));
+            pop_front(&deck);
+        }
+        
+        printf("\nRemaining card: %d\n", front_val(&deck));
+        cleanup_deque(&deck);
     }
 
     return 0;
